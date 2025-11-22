160143e6408e327f9e784614351eaa8c74b410ea
Glaubauf <Thomas.Glaubauf@aerzen.com>
2025-11-22 14:05:51 +0100

    update And Prepare for Button UI Comp

--- libs/localnote/dashboard/src/lib/dashboard/dashboard.ts
+++ libs/localnote/dashboard/src/lib/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Button } from '@local-note/button';
 
 @Component({
   selector: 'lib-dashboard',
-  imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
+  imports: [Button]
 })
 export class Dashboard {}
