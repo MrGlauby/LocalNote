4d6106c95ccbd3b35fa4a161a46aa34e6f80fc27
Glaubauf <Thomas.Glaubauf@aerzen.com>
2025-12-06 17:12:03 +0100

    Update new Button

--- libs/localnote/ui/control/src/lib/button/button.ts
+++ libs/localnote/ui/control/src/lib/button/button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'nl-button',
@@ -6,4 +6,44 @@ import { Component } from '@angular/core';
   templateUrl: './button.html',
   styleUrl: './button.css',
 })
-export class Button {}
+export class Button {
+
+
+  label = input.required<string>();
+  variant = input<'primary' | 'secondary'>('primary');
+  disabled = input<boolean>(false);
+
+  clicked = output<void>();
+
+
+
+  // 5. Computed: Berechnet CSS-Klassen reaktiv basierend auf Inputs
+  protected stylingButton = computed(() => {
+    const classes = ['nl-button', `nl-button-${this.variant()}`];
+    if (this.disabled()) classes.push('nl-disabled');
+    return classes.join(' ');
+  });
+
+
+
+
+
+  // 6. Methode zum Abfangen des Klicks (Disabled Check)
+  /*   protected onClick(event: MouseEvent): void {
+      event.stopPropagation();
+  
+      if (!this.disabled()) {
+        this.clicked.emit();
+      }
+   */
+
+
+  onClick(event: MouseEvent) {
+    event.stopPropagation();
+    if (!this.disabled()) this.clicked.emit();
+  }
+
+}
+
+
+
