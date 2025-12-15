24c8f2a3ebe9128067337737cc8985cdc4c1b7c3
Thomas Glaubauf <mr.glauby@gmail.com>
2025-12-15 20:35:05 +0100

    update UI-button DONE

--- libs/localnote/ui/control/src/lib/button/button.ts
+++ libs/localnote/ui/control/src/lib/button/button.ts
@@ -7,43 +7,36 @@ import { Component, computed, input, output } from '@angular/core';
   styleUrl: './button.css',
 })
 export class Button {
-
-
   label = input.required<string>();
   variant = input<'primary' | 'secondary'>('primary');
   disabled = input<boolean>(false);
-
   clicked = output<void>();
 
-
-
-  // 5. Computed: Berechnet CSS-Klassen reaktiv basierend auf Inputs
+//Wenn das Formular abgefuert wird - resetet sich das Fomular automaitsch durch type!
+type = input <'button' | 'submit' | 'reset' >('button')
+  // klar css klassen im array - habe ich dneke ich verstanden!
   protected stylingButton = computed(() => {
     const classes = ['nl-button', `nl-button-${this.variant()}`];
     if (this.disabled()) classes.push('nl-disabled');
     return classes.join(' ');
   });
 
-
-
-
-
+  // dashier nochmals anschauen!
   // 6. Methode zum Abfangen des Klicks (Disabled Check)
-  /*   protected onClick(event: MouseEvent): void {
-      event.stopPropagation();
-  
-      if (!this.disabled()) {
-        this.clicked.emit();
-      }
-   */
-
-
-  onClick(event: MouseEvent) {
+  protected onClick(event: MouseEvent): void {
     event.stopPropagation();
-    if (!this.disabled()) this.clicked.emit();
-  }
 
+    if (!this.disabled()) {
+      this.clicked.emit();
+    }
+  }
 }
 
 
-
+/* <form>
+  <input name="email" />
+  
+  <nl-button label="Speichern" [type]="'submit'"></nl-button>   <!-- Sendet -->
+  <nl-button label="Zurücksetzen" [type]="'reset'"></nl-button> <!-- Löscht -->
+  <nl-button label="Abbrechen" [type]="'button'"></nl-button>   <!-- Tut nix -->
+</form> */
