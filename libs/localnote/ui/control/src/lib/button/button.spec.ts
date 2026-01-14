947d62631b9aca6a782db3bcdd664adfa34471cc
Thomas Glaubauf <mr.glauby@gmail.com>
2026-01-14 20:56:43 +0100

    update UI-button SPEC-UnitTests

--- libs/localnote/ui/control/src/lib/button/button.spec.ts
+++ libs/localnote/ui/control/src/lib/button/button.spec.ts
@@ -12,10 +12,72 @@ describe('Button', () => {
 
     fixture = TestBed.createComponent(Button);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.componentRef.setInput('label', 'test button');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Create Component - Initialization', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('Outputs', () => {
+    it('should emit clicked event when button is clicked', () => {
+      const spy = jest.spyOn(component.clicked, 'emit');
+      const btn = fixture.nativeElement.querySelector('button');
+      btn.click();
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('Inputs - Rendering', () => {
+    it('should apply [primary] variant by default -  primary', () => {
+      fixture.detectChanges();
+      const btn = fixture.nativeElement.querySelector('button');
+      expect(btn.classList).toContain('nl-button-primary');
+    });
+
+    it('should apply [disabled] variant by disabled btn', () => {
+      fixture.componentRef.setInput('disabled', true);
+      fixture.detectChanges();
+      const btnDisabled = fixture.nativeElement.querySelector('button');
+      expect(btnDisabled.classList).toContain('nl-button-disabled');
+    });
+
+    it('should apply [secondary] variant - btn', () => {
+      fixture.componentRef.setInput('variant', 'secondary');
+      fixture.detectChanges();
+      const btnSecondary = fixture.nativeElement.querySelector('button');
+      expect(btnSecondary.classList).toContain('nl-button-secondary');
+    });
+
+    it('should show the correct [label]', () => {
+      fixture.componentRef.setInput('label', 'testLabel');
+      fixture.detectChanges();
+      const labelBtn = fixture.nativeElement.querySelector('button');
+      expect(labelBtn.textContent.trim()).toBe('testLabel');
+    });
+
+    it('should set input [type] correctly', () => {
+      fixture.componentRef.setInput('type', 'submit');
+      fixture.detectChanges();
+      const btn = fixture.nativeElement.querySelector('button');
+      expect(btn.getAttribute('type')).toBe('submit');
+    });
+  });
+
+  describe('Edge Cases and Errors', () => {
+    it('should NOT emit clicked event when disabled button is clicked', () => {
+      fixture.componentRef.setInput('disabled', true);
+      fixture.detectChanges();
+
+      const spy = jest.spyOn(component.clicked, 'emit');
+      const btn = fixture.nativeElement.querySelector('button');
+      btn.click();
+
+      expect(spy).not.toHaveBeenCalled();
+    });
   });
 });
+
--- libs/localnote/ui/control/src/lib/button/button.ts
+++ libs/localnote/ui/control/src/lib/button/button.ts
@@ -12,17 +12,14 @@ export class Button {
   disabled = input<boolean>(false);
   clicked = output<void>();
 
-//Wenn das Formular abgefuert wird - resetet sich das Fomular automaitsch durch type!
-type = input <'button' | 'submit' | 'reset' >('button')
-  // klar css klassen im array - habe ich dneke ich verstanden!
+type = input <'button' | 'submit' | 'reset' >('button');
+
   protected stylingButton = computed(() => {
     const classes = ['nl-button', `nl-button-${this.variant()}`];
     if (this.disabled()) classes.push('nl-disabled');
     return classes.join(' ');
   });
 
-  // dashier nochmals anschauen!
-  // 6. Methode zum Abfangen des Klicks (Disabled Check)
   protected onClick(event: MouseEvent): void {
     event.stopPropagation();
 
@@ -31,12 +28,3 @@ type = input <'button' | 'submit' | 'reset' >('button')
     }
   }
 }
-
-
-/* <form>
-  <input name="email" />
-  
-  <nl-button label="Speichern" [type]="'submit'"></nl-button>   <!-- Sendet -->
-  <nl-button label="Zurücksetzen" [type]="'reset'"></nl-button> <!-- Löscht -->
-  <nl-button label="Abbrechen" [type]="'button'"></nl-button>   <!-- Tut nix -->
-</form> */
